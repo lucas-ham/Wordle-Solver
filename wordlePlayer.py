1b92c69d88579832f751c124c4b4c6e8691f19c2
Lucas Hamilton <lucas@macobserver.com>
2022-01-28 13:39:47 -0800

    Function to test wordle player
    Runs a generic wordle solver a specified number of times to test it's performance

---
+++ wordlePlayer.py
@@ -0,0 +1,12 @@
+# pass it a function that solves wordle and this will run through it's playing a specified number of times using random words from the solution list
+from wordleFunction import *
+
+def multiPlay(num, func):
+    sum = 0
+    hard = []
+    for i in range(num):
+        curr = func(rawList)
+        sum += curr[0]
+        if curr[0] > 5:
+            hard.append(curr[1])
+    return sum/num, hard
